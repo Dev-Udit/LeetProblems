ffe8e6c9243ebf57f77fc1ced41f16ed27983ed6
UDIT THAPAK <134005576+Dev-Udit@users.noreply.github.com>
2026-02-14 16:58:00 +0530

    Time: 0 ms (100%), Space: 47.2 MB (10.25%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,52 +1,25 @@
 class Solution {
-    class Pair{
-        int row,col;
-        Pair(int r,int c){
-            row =r;
-            col =c;
-        }
+
+    public void dfs(int[][] image, int i, int j, int orgcolor, int newcolor,int n,int m){
+        
+        if(i<0 || j<0 ||i>=n || j>=m ||image[i][j] != orgcolor)return;
+        image[i][j] = newcolor;
+
+        dfs(image,i-1,j,orgcolor,newcolor,n,m);
+        dfs(image,i,j+1,orgcolor,newcolor,n,m);
+        dfs(image,i+1,j,orgcolor,newcolor,n,m);
+        dfs(image,i,j-1,orgcolor,newcolor,n,m);
     }
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int n = image.length;
-        int m = image[0].length;
+        int m =image[0].length;
 
-        int startColor = image[sr][sc];
-        if(startColor == color) return image;
+        int orgcolor = image[sr][sc];
+        if(orgcolor == color)return image;
 
-        Queue<Pair>qu = new LinkedList<>();
-        qu.add(new Pair(sr,sc));
-
-        boolean vis[][] = new boolean[n][m];
-        vis[sr][sc] = true;
-
-        while(!qu.isEmpty()){
-            Pair curr = qu.poll();
-            int i = curr.row;
-            int j =curr.col;
-
-            image[i][j] = color;
-
-            //add just for 4 nbrs
-            if(i-1 >=0 && image[i-1][j]==startColor && !vis[i-1][j]){
-                qu.add(new Pair(i-1,j));
-                vis[i-1][j] = true;
-            }
-            if(j+1 < m && image[i][j+1]==startColor && !vis[i][j+1]){
-                qu.add(new Pair(i,j+1));
-                vis[i][j+1] = true;
-            }
-            if(i+1 <n && image[i+1][j]==startColor && !vis[i+1][j]){
-                qu.add(new Pair(i+1,j));
-                vis[i+1][j] = true;
-            }
-            if(j-1 >=0 && image[i][j-1]==startColor && !vis[i][j-1]){
-                qu.add(new Pair(i,j-1));
-                vis[i][j-1] = true;
-            }
-
-        }
+        dfs(image,sr,sc,orgcolor,color,n,m);
         return image;
-
+        
     }
 }
