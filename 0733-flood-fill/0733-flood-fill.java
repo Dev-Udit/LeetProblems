e816447c1b3b678371559e4aa4092fd5fe6e4351
UDIT THAPAK <134005576+Dev-Udit@users.noreply.github.com>
2026-02-14 16:50:23 +0530

    Time: 2 ms (13.04%), Space: 47.2 MB (10.25%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -11,8 +11,12 @@ class Solution {
         int n = image.length;
         int m = image[0].length;
 
+        int startColor = image[sr][sc];
+        if(startColor == color) return image;
+
         Queue<Pair>qu = new LinkedList<>();
         qu.add(new Pair(sr,sc));
+
         boolean vis[][] = new boolean[n][m];
         vis[sr][sc] = true;
 
@@ -20,24 +24,23 @@ class Solution {
             Pair curr = qu.poll();
             int i = curr.row;
             int j =curr.col;
-            int cell = image[i][j];
-            if(image[i][j]==color)continue;
+
             image[i][j] = color;
 
             //add just for 4 nbrs
-            if(i-1 >=0 && image[i-1][j]==cell && !vis[i-1][j]){
+            if(i-1 >=0 && image[i-1][j]==startColor && !vis[i-1][j]){
                 qu.add(new Pair(i-1,j));
                 vis[i-1][j] = true;
             }
-            if(j+1 < m && image[i][j+1]==cell && !vis[i][j+1]){
+            if(j+1 < m && image[i][j+1]==startColor && !vis[i][j+1]){
                 qu.add(new Pair(i,j+1));
                 vis[i][j+1] = true;
             }
-            if(i+1 <n && image[i+1][j]==cell && !vis[i+1][j]){
+            if(i+1 <n && image[i+1][j]==startColor && !vis[i+1][j]){
                 qu.add(new Pair(i+1,j));
                 vis[i+1][j] = true;
             }
-            if(j-1 >=0 && image[i][j-1]==cell && !vis[i][j-1]){
+            if(j-1 >=0 && image[i][j-1]==startColor && !vis[i][j-1]){
                 qu.add(new Pair(i,j-1));
                 vis[i][j-1] = true;
             }
