0f106ec4d9044a441e479689b572e3371f0ac611
UDIT THAPAK <134005576+Dev-Udit@users.noreply.github.com>
2026-02-14 16:39:39 +0530

    Time: 2 ms (13.04%), Space: 47.2 MB (10.25%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,49 @@
+class Solution {
+    class Pair{
+        int row,col;
+        Pair(int r,int c){
+            row =r;
+            col =c;
+        }
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int n = image.length;
+        int m = image[0].length;
+
+        Queue<Pair>qu = new LinkedList<>();
+        qu.add(new Pair(sr,sc));
+        boolean vis[][] = new boolean[n][m];
+        vis[sr][sc] = true;
+
+        while(!qu.isEmpty()){
+            Pair curr = qu.poll();
+            int i = curr.row;
+            int j =curr.col;
+            int cell = image[i][j];
+            if(image[i][j]==color)continue;
+            image[i][j] = color;
+
+            //add just for 4 nbrs
+            if(i-1 >=0 && image[i-1][j]==cell && !vis[i-1][j]){
+                qu.add(new Pair(i-1,j));
+                vis[i-1][j] = true;
+            }
+            if(j+1 < m && image[i][j+1]==cell && !vis[i][j+1]){
+                qu.add(new Pair(i,j+1));
+                vis[i][j+1] = true;
+            }
+            if(i+1 <n && image[i+1][j]==cell && !vis[i+1][j]){
+                qu.add(new Pair(i+1,j));
+                vis[i+1][j] = true;
+            }
+            if(j-1 >=0 && image[i][j-1]==cell && !vis[i][j-1]){
+                qu.add(new Pair(i,j-1));
+                vis[i][j-1] = true;
+            }
+
+        }
+        return image;
+
+    }
+}
