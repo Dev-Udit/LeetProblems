9f44b2dd6c7a0a869eb946d7ac3f95e25542b357
UDIT THAPAK <134005576+Dev-Udit@users.noreply.github.com>
2026-02-16 01:14:45 +0530

    Time: 1 ms (60.76%), Space: 47.7 MB (82.54%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,26 +1,23 @@
 class Solution {
     public int trap(int[] height) {
+        int n = height.length;
+        int leftMax = Integer.MIN_VALUE;
+        int rightMax = Integer.MIN_VALUE;
         int left = 0;
-        int right = height.length-1;
-        int left_max = 0;
-        int right_max =0 ;
-
-        int water = 0;
-        while(left<right){
-            left_max = Math.max(left_max,height[left]);
-            right_max = Math.max(right_max,height[right]);
-
-
-            //The smaller side decides the water
-            if(left_max < right_max){
-                water += left_max - height[left];
-                left++;
-            }
+        int right = n-1;
+        int ans = 0;
+        while(left < right){
+            leftMax = Math.max(leftMax , height[left]);
+            rightMax = Math.max(rightMax , height[right]);
+            if(leftMax < rightMax){
+                ans += leftMax - height[left++];
+            } 
             else {
-                water += right_max - height[right];
-                right--;
+                ans += rightMax - height[right--];
             }
         }
-        return water;
+        return ans;
+
+        
     }
 }
